3f13774e3269387eda880ab7f3d4a66539746da5
asunder123 <anand.sunder@capgemini.com>
2021-10-18 20:38:31 +0530

    Changes

--- testjar.sh
+++ testjar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 ls -ltr
-cmd="java -jar *.jar"
+cmd="$HOME/target/java -jar *.jar"
 status=$?
 [ $status -eq 0 ] && echo "$cmd command was successful" || echo "$cmd failed"
 cmd="curl http://localhost:8082/actuator/chaosmonkey"
